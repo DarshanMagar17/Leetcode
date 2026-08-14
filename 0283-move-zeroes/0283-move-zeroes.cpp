bb15f3e27e4804c65a5deff9f5511141f229fe4b
DarshanMagar17 <darshanm12344321@gmail.com>
2026-08-14 22:03:33 +0530

    Time: 11 ms (7.48%), Space: 24 MB (7.86%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[j]==0)
+            {
+                nums.push_back(nums[j]);
+                nums.erase(nums.begin()+j);
+            }
+            else
+            {
+                j++;
+            }
+        }
+        
+    }
+};
